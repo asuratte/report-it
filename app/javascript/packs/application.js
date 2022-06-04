04f67f86ef426d914cadd63b935c24bc58068bf6
Amber Suratte <as00193@my.westga.edu>
2022-06-04 15:59:15 -0400

    Installed and configured Bootstrap 5. Created blank homepage to confirm Bootstrap is configured correctly.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,24 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("@popperjs/core")
+
+import "bootstrap"
+
+// Import the specific modules you may need (Modal, Alert, etc)
+import { Button, Alert, Collapse, Dropdown, Modal, Tab, Tooltip, Popover, Scrollspy } from "bootstrap"
+
+require("../stylesheets/application.scss")
+
+document.addEventListener("turbolinks:load", () => {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
