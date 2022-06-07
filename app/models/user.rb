f844901cdeaf3400154625303d7c28c391f99b54
Amber Suratte <as00193@my.westga.edu>
2022-06-07 18:44:46 -0500

    Added is_resident?, is_official?, is_admin? methods to User model for determining user role.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum role: [:resident, :official, :admin]       
+  enum role: [:resident, :official, :admin]
+  
+  def is_resident?
+    return self.role == 'resident'
+  end
+
+  def is_official?
+    return self.role == 'official'
+  end
+
+  def is_admin?
+    return self.role == 'admin'
+  end
 end
