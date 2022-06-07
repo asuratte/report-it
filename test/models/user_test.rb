11fa3a4685179f0f9f22dc155759ed3c4f40ea5b
Amber Suratte <as00193@my.westga.edu>
2022-06-07 18:43:09 -0500

    Added 2 additional user test fixtures. Added unit tests for User is_admin?, is_official?, and is_resident? methods.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @resident_user = users(:one)
+    @official_user = users(:two)
+    @admin_user = users(:three)
+  end
+
+  test "should return true for user one is_resident?" do
+    assert @resident_user.is_resident?
+  end
+
+  test "should return false for user one is_admin?" do
+    assert_equal false, @resident_user.is_admin?
+  end
+
+  test "should return false for user one is_official?" do
+    assert_equal false, @resident_user.is_official?
+  end
+
+  test "should return true for user two is_official?" do
+    assert @official_user.is_official?
+  end
+
+  test "should return false for user two is_admin?" do
+    assert_equal false, @official_user.is_admin?
+  end
+
+  test "should return false for user two is_resident?" do
+    assert_equal false, @official_user.is_resident?
+  end
+
+  test "should return true for user three is_admin?" do
+    assert @admin_user.is_admin?
+  end
+
+  test "should return false for user three is_official?" do
+    assert_equal false, @admin_user.is_official?
+  end
+
+  test "should return false for user three is_resident?" do
+    assert_equal false, @admin_user.is_resident?
+  end
+
 end
