94c5ae89c5c265741f655a42c436a7e731b41986
Amber Suratte <as00193@my.westga.edu>
2022-06-08 18:25:57 -0500

    Updated registration edit form to use bootstrap and have additional fields needed for user model. Updated registrations controller logic to work for both create and update registration.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
-    private
-      def sign_up_params
-        params.require(:user).permit(:email, :password, :first_name, :last_name, :address1, :city, :state, :zip, :phone, :username)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :first_name, :last_name, :address1, :address2, :city, :state, :zip, :phone, :username])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :first_name, :last_name, :address1, :address2, :city, :state, :zip, :phone, :username])
       end
+
   end
