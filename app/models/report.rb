66c3b1e4426b72358576f4a94841aa4731ab4b3d
Mike Bastarache <mb00147@my.westga.edu>
2022-06-08 19:09:09 -0400

    Added length validation to report model and corresponding unit test

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,7 @@
 class Report < ApplicationRecord
   belongs_to :user
   validates :city, :state, :zip, :description, :category, :subcategory, presence: true
+  validates :address1, :address2, :city, :state, length: { maximum: 50 }
+  validates :zip, length: { is: 5 }
+  validates :description, length: {maximum: 1000}
 end
--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class ReportTest < ActiveSupport::TestCase
+
+  setup do
+    @report = reports(:one)
+  end
+
   test "report attributes cannot be empty" do
     report = Report.new
     assert report.invalid?
@@ -11,4 +16,22 @@ class ReportTest < ActiveSupport::TestCase
     assert report.errors[:category].any?
     assert report.errors[:subcategory].any?
   end
+
+  test "report is not valid if attributes are over maximum characters allowed" do
+    assert @report.valid?
+    @report.address1 = "a" * 51
+    @report.address2 = "a" * 51
+    @report.city = "a" * 51
+    @report.state = "a" * 51
+    @report.zip = "0" * 6
+    @report.description = "a" * 1001
+    assert @report.invalid?
+    assert @report.errors[:address1].any?
+    assert @report.errors[:address2].any?
+    assert @report.errors[:zip].any?
+    assert @report.errors[:city].any?
+    assert @report.errors[:state].any?
+    assert @report.errors[:description].any?
+  end
+
 end
