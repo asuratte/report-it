e623b3003121a0a45718641f5b0369ffa2317463
Amber Suratte <as00193@my.westga.edu>
2022-06-09 21:05:12 -0500

    Updated Users controller to remove destroy action. Updated navbar to include logic for admin users to hide admin links from non-admins. Updated routes to restrict all user creating, reading, updating that is not related to devise to admins only.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, :path_prefix => 'account', :controllers => {
     registrations: 'registrations'
   }
-  resources :users
+  authenticate :user, -> (user) { user.is_admin? } do
+    resources :users
+  end
 
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
