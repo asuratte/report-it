b41657d81a0f7233d95ac0038e892237e2b2cdcc
Amber Suratte <as00193@my.westga.edu>
2022-06-09 21:18:21 -0500

    Updated users controller unit tests to log in as an admin user first so routes can be properly accesed.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,21 +1,27 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  
   setup do
-    @user = users(:one)
+    @admin_user = users(:three)
+    @user2 = users(:two)
   end
 
   test "should get index" do
+    sign_in @admin_user
     get users_url
     assert_response :success
   end
 
   test "should get new" do
+    sign_in @admin_user
     get new_user_url
     assert_response :success
   end
 
   test "should create user" do
+    sign_in @admin_user
     assert_difference('User.count') do
       post users_url, params: { user: { first_name: 'Sally', last_name: "Thompson", address1: '123 West St.', city: 'Omaha', state: 'NE', zip: '50099', phone: '333-444-5555', username: 'test_username', active: 'true', role: 'resident', email: 'test_email@email.com', password: 'test123' } }
     end
@@ -23,18 +29,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show user" do
-    get user_url(@user)
+    sign_in @admin_user
+    get user_url(@user2)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_user_url(@user)
+    sign_in @admin_user
+    get edit_user_url(@user2)
     assert_response :success
   end
 
   test "should update user" do
-    patch user_url(@user), params:  { user: { first_name: 'Charles', last_name: @user.last_name, address1: @user.address1, address2: @user.address2, city: @user.city, state: @user.state, zip: @user.zip, phone: @user.phone, username: @user.username, active: @user.active, role: @user.role, email: @user.email, password: @user.password } }
-    assert_redirected_to user_url(@user)
+    sign_in @admin_user
+    patch user_url(@user2), params:  { user: { first_name: 'Charles', last_name: @user2.last_name, address1: @user2.address1, address2: @user2.address2, city: @user2.city, state: @user2.state, zip: @user2.zip, phone: @user2.phone, username: @user2.username, active: @user2.active, role: @user2.role, email: @user2.email, password: @user2.password } }
+    assert_redirected_to user_url(@user2)
   end
 
 end
