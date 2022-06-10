0819a5dfeb484b3648013d4b0297114f470253ea
Mike Bastarache <mb00147@my.westga.edu>
2022-06-10 12:59:06 -0400

    Created resident controller and updated routes to get resident home page.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     registrations: 'registrations'
   }
 
+  authenticate :user, -> (user) { user.is_resident? } do
+    get 'resident', to: 'resident#index'
+  end
+  
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
