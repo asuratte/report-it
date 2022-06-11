ee93e6d2d3d7147dc0d55363555000a4e879b280
Mike Bastarache <mb00147@my.westga.edu>
2022-06-11 16:22:57 -0400

    Updated columns and styling in reports index table

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,7 @@ class ReportsController < ApplicationController
 
   # GET /reports or /reports.json
   def index
-    @reports = Report.all
+    @reports = Report.all.order('created_at DESC')
   end
 
   # GET /reports/1 or /reports/1.json
