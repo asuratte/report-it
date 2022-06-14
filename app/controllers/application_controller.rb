7d64594d18d0089af58477f190a14a9033596774
Kim Weible <kw00183@my.westga.edu>
2022-06-14 07:56:45 -0400

    expose official path to admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if current_user.is_resident?
       resident_path
-    elsif current_user.is_official?
+    elsif current_user.is_official? || current_user.is_admin?
       official_path
     else
       root_path
