b2af16ebf35554f445cf10fc320ef6b3b955c392
Amber Suratte <as00193@my.westga.edu>
2022-06-15 07:35:03 -0500

    Added additional unit tests for User model to cover validation of user properties during user creation. Added uniqueness validation to User model for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :address1, :city, :state, :zip, :phone, :username, presence: true
   validates_format_of :zip, :with => /\A\d{5}(-\d{4})?\z/, :message => "should be a valid US zip code. ex: 12345 or 12345-1234"
   validates_format_of :phone,  with: /\A(1-)?\d{3}-\d{3}-\d{4}\z/, :message => "should be a valid phone number with dashes. ex: 111-222-3333, 1-333-444-5555"
-  validates :username, length: {minimum: 6, maximum: 30}
+  validates :username, length: {minimum: 6, maximum: 30}, uniqueness: true
   has_many :reports
 
   def active_for_authentication?
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -61,4 +61,92 @@ class UserTest < ActiveSupport::TestCase
     assert @admin_user.is_active?
   end
 
+  test "user attributes cannot be empty" do
+    user = User.new
+    assert user.invalid?
+    assert user.errors[:city].any?
+    assert user.errors[:state].any?
+    assert user.errors[:zip].any?
+    assert user.errors[:phone].any?
+    assert user.errors[:username].any?
+    assert user.errors[:first_name].any?
+    assert user.errors[:last_name].any?
+  end
+
+  test "user should not be created if zip, phone, and username do not pass validation" do
+    user1 = User.new
+    user1.phone = "asdfghjklzxc"
+    user1.zip = "asdfghjklzxc"
+    user1.username = "a" * 31
+    user1.first_name = @resident_user.first_name
+    user1.last_name = @resident_user.last_name
+    user1.address1 = @resident_user.address1
+    user1.city = @resident_user.city
+    user1.state = @resident_user.state
+    user1.password = '12345678'
+    user1.role = @resident_user.role
+    user1.active = @resident_user.active
+    user1.email = 'newuser@gmail.com'
+    assert user1.invalid?
+    assert user1.errors[:phone].any?
+    assert user1.errors[:username].any?
+    assert user1.errors[:zip].any?
+    user2 = User.new
+    user2.phone = "4445553333"
+    user2.zip = "600978"
+    user2.username = "asdf"
+    user2.first_name = @resident_user.first_name
+    user2.last_name = @resident_user.last_name
+    user2.address1 = @resident_user.address1
+    user2.city = @resident_user.city
+    user2.state = @resident_user.state
+    user2.password = '12345678'
+    user2.role = @resident_user.role
+    user2.active = @resident_user.active
+    user2.email = 'newuser@gmail.com'
+    assert user2.invalid?
+    assert user2.errors[:phone].any?
+    assert user2.errors[:username].any?
+    assert user2.errors[:zip].any?
+  end
+
+  test "user should be created if zip, phone, and username pass validation and all required fields are present" do
+    user1 = User.new
+    user1.first_name = @resident_user.first_name
+    user1.last_name = @resident_user.last_name
+    user1.address1 = @resident_user.address1
+    user1.city = @resident_user.city
+    user1.state = @resident_user.state
+    user1.phone = @resident_user.phone
+    user1.zip = @resident_user.zip
+    user1.username = 'asdffghjkl'
+    user1.password = '12345678'
+    user1.role = @resident_user.role
+    user1.active = @resident_user.active
+    user1.email = 'newuser@gmail.com'
+    assert user1.valid?
+    assert !user1.errors[:phone].any?
+    assert !user1.errors[:username].any?
+    assert !user1.errors[:zip].any?
+  end
+
+  test "user should not be created if email or username are already registered to another user" do
+    user1 = User.new
+    user1.first_name = @resident_user.first_name
+    user1.last_name = @resident_user.last_name
+    user1.address1 = @resident_user.address1
+    user1.city = @resident_user.city
+    user1.state = @resident_user.state
+    user1.phone = @resident_user.phone
+    user1.zip = @resident_user.zip
+    user1.username = @resident_user.username
+    user1.password = '12345678'
+    user1.role = @resident_user.role
+    user1.active = @resident_user.active
+    user1.email = @resident_user.email
+    assert user1.invalid?
+    assert user1.errors[:email].any?
+    assert user1.errors[:username].any?
+  end
+ 
 end
