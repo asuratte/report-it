943c07ee733f6a3d4060c86556071a26f0825cef
Kim Weible <kw00183@my.westga.edu>
2022-06-18 10:22:01 -0400

    update name of current theme on show html with id; change th on theme index; update fixtures on theme.yml; update unit tests on themes controller test

--- test/controllers/themes_controller_test.rb
+++ test/controllers/themes_controller_test.rb
@@ -1,48 +1,119 @@
 require "test_helper"
 
 class ThemesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @theme = themes(:one)
+    @resident_user = users(:one)
+    @official_user = users(:two)
+    @admin_user = users(:three)
   end
 
-  test "should get index" do
+  test "should get index if admin" do
+    sign_in @admin_user
     get themes_url
     assert_response :success
   end
 
-  test "should get new" do
-    get new_theme_url
+  test "should not get index if resident or official" do
+    sign_in @resident_user
+    get themes_url
+    assert_response :redirect
+
+    sign_in @official_user
+    get themes_url
+    assert_response :redirect
+  end
+
+  test "should allow view of new if admin (new is current theme)" do
+    sign_in @admin_user
+    get '/themes/new'
     assert_response :success
   end
 
-  test "should create theme" do
-    assert_difference('Theme.count') do
-      post themes_url, params: { theme: { name: @theme.name } }
-    end
+  test "should not allow view of new if resident or official" do
+    sign_in @resident_user
+    get '/themes/new'
+    assert_response :redirect
 
-    assert_redirected_to theme_url(Theme.last)
+    sign_in @official_user
+    get '/themes/new'
+    assert_response :redirect
   end
 
-  test "should show theme" do
+  test "should not create theme" do
+    sign_in @admin_user
+    assert_difference('Theme.count', 0) do
+      get themes_url, params: { theme: { name: @theme.name } }
+    end
+  end
+
+  test "should show theme if admin" do
+    sign_in @admin_user
     get theme_url(@theme)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should not show theme if resident or official" do
+    sign_in @resident_user
+    get theme_url(@theme)
+    assert_response :redirect
+
+    sign_in @official_user
+    get theme_url(@theme)
+    assert_response :redirect
+  end
+
+  test "should get edit if admin" do
+    sign_in @admin_user
     get edit_theme_url(@theme)
     assert_response :success
   end
 
-  test "should update theme" do
-    patch theme_url(@theme), params: { theme: { name: @theme.name } }
-    assert_redirected_to theme_url(@theme)
+  test "should not get edit if resident or official" do
+    sign_in @resident_user
+    get edit_theme_url(@theme)
+    assert_response :redirect
+
+    sign_in @official_user
+    get edit_theme_url(@theme)
+    assert_response :redirect
   end
 
-  test "should destroy theme" do
-    assert_difference('Theme.count', -1) do
-      delete theme_url(@theme)
-    end
+  test "should update theme" do
+    sign_in @admin_user
 
-    assert_redirected_to themes_url
+    get '/themes/1/edit'
+    patch theme_url(@theme), params: { theme: { name: "Theme 1" } }
+    assert_response :redirect
+    get '/themes/1'
+    assert_select "span#current_theme", text: "Theme 1"
+
+    get '/themes/1/edit'
+    patch theme_url(@theme), params: { theme: { name: "Theme 2" } }
+    assert_response :redirect
+    get '/themes/1'
+    assert_select "span#current_theme", text: "Theme 2"
+
+    get '/themes/1/edit'
+    patch theme_url(@theme), params: { theme: { name: "Theme 3" } }
+    assert_response :redirect
+    get '/themes/1'
+    assert_select "span#current_theme", text: "Theme 3"
+  end
+
+  test "should not allow anyone to destroy" do
+    sign_in @resident_user
+    get '/theme/1/destroy'
+    assert_response :redirect
+
+    sign_in @official_user
+    get '/theme/1/destroy'
+    assert_response :redirect
+
+    sign_in @admin_user
+    get '/theme/1/destroy'
+    assert_response :redirect
   end
 end
