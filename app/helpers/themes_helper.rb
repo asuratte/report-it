fd402ba66f84f8a8668438a6325d1ca17149273c
Kim Weible <kw00183@my.westga.edu>
2022-06-18 02:58:08 -0400

    add in description and image to theme show html; update the theme form to a dropdown using helper methods; add helper method to get current theme; add in palette style to theme stylesheets; update application html to switch theme stylesheet when user changes theme

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -1,9 +1,9 @@
 module ThemesHelper
   def themes
     @themes = [
-      { "name" => "Theme 1", "description" => "Default - Orange and Gray", "image" => "theme1_palette.png" },
-      { "name" => "Theme 2", "description" => "Teal and Burgundy", "image" => "theme2_palette.png" },
-      { "name" => "Theme 3", "description" => "Burgundy and Green", "image" => "theme3_palette.png" }
+      { "id" => 1, "name" => "Theme 1", "description" => "Default - Orange and Gray", "image" => "theme1_palette.png" },
+      { "id" => 2, "name" => "Theme 2", "description" => "Teal and Burgundy", "image" => "theme2_palette.png" },
+      { "id" => 3, "name" => "Theme 3", "description" => "Burgundy and Green", "image" => "theme3_palette.png" }
     ]
   end
 
@@ -15,5 +15,14 @@ module ThemesHelper
   def get_image_by_name(name)
     @item = themes.find {|theme| theme["name"] == name }
     @item["image"]
+  end
+
+  def get_current_theme
+    @current_theme = Theme.first.name
+  end
+
+  def get_id_by_name(name)
+    @item = themes.find {|theme| theme["name"] == name }
+    @item["id"]
   end
 end
