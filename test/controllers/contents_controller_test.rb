2f2054c2f3940d277464e6ad40cf203e5f489a34
Amber Suratte <as00193@my.westga.edu>
2022-06-19 20:06:55 -0500

    Added additional Content controller unit tests to ensure correct routing

--- test/controllers/contents_controller_test.rb
+++ test/controllers/contents_controller_test.rb
@@ -6,24 +6,56 @@ class ContentsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @content = contents(:one)
     @admin_user = users(:three)
+    @resident_user = users(:one)
+    @official_user = users(:two)
   end
 
-  test "should get index" do
+  test "should get index if admin" do
     sign_in @admin_user
     get contents_url
     assert_response :success
   end
 
-  test "should show content" do
+  test "should not get index if resident or official" do
+    sign_in @resident_user
+    get contents_url
+    assert_response :redirect
+
+    sign_in @official_user
+    get contents_url
+    assert_response :redirect
+  end
+
+  test "should show content if admin" do
     sign_in @admin_user
     get content_url(@content)
     assert_response :success
+  end
+
+  test "should not show content if resident or official" do
+    sign_in @resident_user
+    get content_url(@content)
+    assert_response :redirect
+
+    sign_in @official_user
+    get content_url(@content)
+    assert_response :redirect
   end
 
   test "should get edit" do
     sign_in @admin_user
     get edit_content_url(@content)
     assert_response :success
+  end
+
+  test "should not get edit if resident or official" do
+    sign_in @resident_user
+    get edit_content_url(@content)
+    assert_response :redirect
+
+    sign_in @official_user
+    get edit_content_url(@content)
+    assert_response :redirect
   end
 
   test "should update content" do
