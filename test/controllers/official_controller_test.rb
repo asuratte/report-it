5fc4a9272311ceb479f994de89bf95ffdc2a79d8
Kim Weible <kw00183@my.westga.edu>
2022-06-22 19:16:48 -0400

    add id to strong tag on report form; add in additional unit tests to check the edit report page shows incident number based on role

--- test/controllers/official_controller_test.rb
+++ test/controllers/official_controller_test.rb
@@ -68,7 +68,7 @@ class OfficialControllerTest < ActionDispatch::IntegrationTest
     assert_select "label#severity_label", text: "Severity"
   end
 
-  test "resident should not see incident number on report" do
+  test "resident should not see incident number on report show" do
     get '/users/sign_in'
     sign_in @resident_user
 
@@ -77,7 +77,7 @@ class OfficialControllerTest < ActionDispatch::IntegrationTest
     assert_select "strong#incident_number", false, text: "Incident Number:"
   end
 
-  test "official should see incident number on report" do
+  test "official should see incident number on report show" do
     get '/users/sign_in'
     sign_in @official_user
     get official_url
@@ -88,13 +88,44 @@ class OfficialControllerTest < ActionDispatch::IntegrationTest
     assert_select "strong#incident_number", text: "Incident Number:"
   end
 
-  test "admin should see incident number on report" do
+  test "admin should see incident number on report show" do
     get '/users/sign_in'
     sign_in @admin_user
     get official_url
     assert_response :success
 
     get '/reports/1'
+    assert_response :success
+    assert_select "strong#incident_number", text: "Incident Number:"
+  end
+
+  test "resident should not see incident number on report edit" do
+    get '/users/sign_in'
+    sign_in @resident_user
+
+    get '/reports/1/edit'
+    assert_response :success
+    assert_select "strong#incident_number", false, text: "Incident Number:"
+  end
+
+  test "official should see incident number on report edit" do
+    get '/users/sign_in'
+    sign_in @official_user
+    get official_url
+    assert_response :success
+
+    get '/reports/1/edit'
+    assert_response :success
+    assert_select "strong#incident_number", text: "Incident Number:"
+  end
+
+  test "admin should see incident number on report edit" do
+    get '/users/sign_in'
+    sign_in @admin_user
+    get official_url
+    assert_response :success
+
+    get '/reports/1/edit'
     assert_response :success
     assert_select "strong#incident_number", text: "Incident Number:"
   end
