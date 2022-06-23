ad23554257c990c3d3fb4fee45c9d7478b6873a7
Mike Bastarache <mb00147@my.westga.edu>
2022-06-23 18:40:52 -0400

    Updated error message for image validation

--- app/models/report.rb
+++ app/models/report.rb
@@ -8,7 +8,7 @@ class Report < ApplicationRecord
   validates :description, length: {maximum: 1000}
   validates_format_of :zip, :with => /\A\d{5}(-\d{4})?\z/, :message => "should be a valid US zip code. ex: 12345 or 12345-1234"
   validates :image, content_type: [:png, :jpg, :jpeg]
-  validates :image, size: { between: 1.kilobyte..5.megabytes , message: 'must be less than 5 MB' }
+  validates :image, size: { between: 1.kilobyte..5.megabytes , message: 'must be between 1 KB and 5 MB' }
 
   def is_active?
       return self.active_status == 'active' ? true : false
