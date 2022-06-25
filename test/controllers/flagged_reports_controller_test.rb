ec87397f10234947ea0327b2e336b8540d4cd55d
Amber Suratte <as00193@my.westga.edu>
2022-06-25 15:40:00 -0500

    Added unit tests to confirm appropriate access for user roles of flagged reports page.

--- test/controllers/flagged_reports_controller_test.rb
+++ test/controllers/flagged_reports_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class FlaggedReportsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @resident_user = users(:one)
+    @official_user = users(:two)
+    @admin_user = users(:three)
+  end
+
+  test "admin user should get flagged-reports" do
+    sign_in @admin_user
+    get flagged_reports_url
+    assert_response :success
+  end
+
+  test "official and resident users should not get flagged-reports" do
+    sign_in @official_user
+    get flagged_reports_url
+    assert_response :redirect
+    sign_out @official_user
+    sign_in @resident_user
+    get flagged_reports_url
+    assert_response :redirect
+  end
+
 end
