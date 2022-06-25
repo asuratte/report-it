dc7028ebdf7782058de617ce6d316c5eaf4797d7
Amber Suratte <as00193@my.westga.edu>
2022-06-25 15:31:02 -0500

    Generated controller for flagged-reports page, added route for admins only to access it, added view with table of flagged reports. Updated view of deactivated reports so if there are no reports it shows a message indicating such.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_225911) do
+ActiveRecord::Schema.define(version: 2022_06_25_011656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_225911) do
     t.string "subcategory"
     t.string "status", default: "New"
     t.string "severity", default: "Not Set"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "active_status", default: 0
---
+++ test/controllers/flagged_reports_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FlaggedReportsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
