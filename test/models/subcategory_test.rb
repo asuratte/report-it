ce5ca096ea0d0707f4706cb9daa02ba381ad6907
Amber Suratte <as00193@my.westga.edu>
2022-06-28 06:47:26 -0500

    Added get_subcategories_by_category class method to Subcagtegory model for retrieving list of subcategories by category. Added model unit tests for it and updated fixtures.

--- test/models/subcategory_test.rb
+++ test/models/subcategory_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class SubcategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @category1 = categories(:one)
+    @category2 = categories(:two)
+    @subcategory1 = subcategories(:one)
+    @subcategory2 = subcategories(:two)
+  end
+
+  test "Should get 'Pothole complaint' and 'Road hazard complaint' when getting subcategories by category 'Transportation and streets'" do 
+    subcategories = [@subcategory1, @subcategory2]
+    assert_equal @category1.name, "Transportation and streets"
+    assert_equal @subcategory1.name, "Pothole complaint"
+    assert_equal @subcategory2.name, "Road hazard complaint"
+    assert_equal Subcategory.get_subcategories_by_category(@category1), subcategories
+  end
+
+  test "Should get empty array when getting subcategories by category that does not have any subcategories" do 
+    assert_equal Subcategory.get_subcategories_by_category(@category2), []
+  end
+
 end
