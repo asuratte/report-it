dcfa1278ce2b3ff1057c75e44cf84da2908da54c
Amber Suratte <as00193@my.westga.edu>
2022-06-28 13:03:53 -0500

    Added get_active_categories class method to Category class and added unit test and fixture.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
     has_many :subcategory
+
+    def self.get_active_categories
+        self.all.where(:active => true)
+    end
+
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @category1 = categories(:one)
+    @category2 = categories(:two)
+  end
+
+  test "Should get 'Transportation and streets' and 'Animals' but not 'Sidewalks' when getting active categories" do 
+    categories = [@category1, @category2]
+    assert_equal @category1.name, "Transportation and streets"
+    assert_equal @category2.name, "Animals"
+    assert_equal Category.get_active_categories, categories
+  end
+
 end
