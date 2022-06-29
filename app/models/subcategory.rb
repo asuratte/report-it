51caacf7969c93f679620cd49a3102b9bedafe10
Amber Suratte <as00193@my.westga.edu>
2022-06-29 07:34:36 -0500

    Added get_active_subcategories class method to Subcategories model, added unit test and fixtures, updated categories model unit tests after fixtures changed

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,6 +1,10 @@
 class Subcategory < ApplicationRecord
   belongs_to :category, :optional => true
 
+  def self.get_active_subcategories
+    self.all.where(:active => true)
+  end
+
   def self.get_active_subcategories_by_category(category)
     self.all.where(:category_id => category.id, :active => true)
   end
