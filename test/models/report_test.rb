437adb9d5ef85d7a020303d588e1a565c5013478
Mike Bastarache <mb00147@my.westga.edu>
2022-06-29 12:09:26 -0400

    Created unit tests for searching by category or subcategory

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -207,4 +207,17 @@ class ReportTest < ActiveSupport::TestCase
     assert_equal 3, report.last.id
   end
 
+  test "search finds reports by category" do
+    report = Report.search("Category", "Trash, Recycling, and Pickup")
+    assert_equal 2, report.count
+    assert_equal 1, report.first.id
+    assert_equal 5, report.last.id
+  end
+
+  test "search finds reports by subcategory" do
+    report = Report.search("Category", "Sewer complaint")
+    assert_equal 1, report.count
+    assert_equal 4, report.first.id
+  end
+
 end
