251b0ef7bf6b691fa9dc6a4eb0c0d2db00e1af80
Kim Weible <kw00183@my.westga.edu>
2022-06-29 21:34:56 -0400

    add in the additional admin flagged and deactivated search pages to the search form partial; update the admin deactivated and flagged index html with the partial render tag; update the deactivated and flagged controllers index method with the search parameters and add in the get_search_values method to both

--- app/controllers/deactivated_reports_controller.rb
+++ app/controllers/deactivated_reports_controller.rb
@@ -1,14 +1,27 @@
 class DeactivatedReportsController < ApplicationController
     rescue_from Pagy::OverflowError, with: :redirect_to_last_page
     rescue_from Pagy::VariableError, with: :redirect_to_last_page
+    before_action :get_search_values, only: [:index]
 
     def index
-        @pagy, @deactivated_reports = pagy(Report.all.where.not(active_status: 0).order('created_at DESC'), items: 10, size: [1,0,0,1])
+      @form_submit_path = '/deactivated-reports'
+      @admin_deactivated_search_type = session[:admin_deactivated_search_type]
+      @admin_deactivated_search_term = session[:admin_deactivated_search_term]
+      @pagy, @deactivated_reports = pagy(Report.order('created_at DESC').search(session[:admin_deactivated_search_type], session[:admin_deactivated_search_term]).where.not(active_status: 0), items: 10, size: [1,0,0,1])
+      # @pagy, @deactivated_reports = pagy(Report.all.where.not(active_status: 0).order('created_at DESC'), items: 10, size: [1,0,0,1])
     end
 
     private
     # Redirects to the last page when exception thrown
     def redirect_to_last_page(exception)
         redirect_to url_for(page: exception.pagy.last)
+    end
+
+    # Sets the search type and term for the session using the search parameters
+    def get_search_values
+      if params[:admin_deactivated_search_term]
+        session[:admin_deactivated_search_type] = params[:admin_deactivated_search_type]
+        session[:admin_deactivated_search_term] = params[:admin_deactivated_search_term]
+      end
     end
 end
--- app/controllers/flagged_reports_controller.rb
+++ app/controllers/flagged_reports_controller.rb
@@ -1,15 +1,28 @@
 class FlaggedReportsController < ApplicationController
     rescue_from Pagy::OverflowError, with: :redirect_to_last_page
     rescue_from Pagy::VariableError, with: :redirect_to_last_page
+    before_action :get_search_values, only: [:index]
 
     def index
-        @pagy, @flagged_reports = pagy(Report.all.where(status: "Flagged", active_status: "active").order('created_at DESC'), items: 10, size: [1,0,0,1])
+      @form_submit_path = '/flagged-reports'
+      @admin_flagged_search_type = session[:admin_flagged_search_type]
+      @admin_flagged_search_term = session[:admin_flagged_search_term]
+      @pagy, @flagged_reports = pagy(Report.order('created_at DESC').search(session[:admin_flagged_search_type], session[:admin_flagged_search_term]).where(status: "Flagged", active_status: "active"), items: 10, size: [1,0,0,1])
+      #@pagy, @flagged_reports = pagy(Report.all.where(status: "Flagged", active_status: "active").order('created_at DESC'), items: 10, size: [1,0,0,1])
     end
 
     private
     # Redirects to the last page when exception thrown
     def redirect_to_last_page(exception)
         redirect_to url_for(page: exception.pagy.last)
+    end
+
+    # Sets the search type and term for the session using the search parameters
+    def get_search_values
+      if params[:admin_flagged_search_term]
+        session[:admin_flagged_search_type] = params[:admin_flagged_search_type]
+        session[:admin_flagged_search_term] = params[:admin_flagged_search_term]
+      end
     end
 
 end
