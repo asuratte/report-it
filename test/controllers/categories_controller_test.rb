6a614c75942bfb51889c1bfff41f075d8c382358
Amber Suratte <as00193@my.westga.edu>
2022-07-02 16:53:12 -0500

    Added additional tests to categories controller to confirm access for different roles

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,18 +6,46 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @category = categories(:one)
     @admin_user = users(:three)
+    @resident_user = users(:one)
+    @official_user = users(:two)
   end
 
-  test "should get index" do
+  test "should get index if admin" do
     sign_in @admin_user
     get categories_url
     assert_response :success
   end
 
-  test "should get new" do
+  test "should not get index if official, resident, or unauthenticated" do
+    get categories_url
+    assert_response :redirect
+    sign_in @official_user
+    get categories_url
+    assert_response :redirect
+    sign_out @official_user
+    sign_in @resident_user
+    get categories_url
+    assert_response :redirect
+    sign_out @resident_user
+  end
+
+  test "should get new if admin" do
     sign_in @admin_user
     get new_category_url
     assert_response :success
+  end
+
+  test "should not get new if official, resident, or unauthenticated" do
+    get new_category_url
+    assert_response :redirect
+    sign_in @official_user
+    get new_category_url
+    assert_response :redirect
+    sign_out @official_user
+    sign_in @resident_user
+    get new_category_url
+    assert_response :redirect
+    sign_out @resident_user
   end
 
   test "should create category" do
@@ -29,16 +57,42 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to category_url(Category.last)
   end
 
-  test "should show category" do
+  test "should show category if admin" do
     sign_in @admin_user
     get category_url(@category)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should not show category if official, resident, or unauthenticated" do
+    get category_url(@category)
+    assert_response :redirect
+    sign_in @official_user
+    get category_url(@category)
+    assert_response :redirect
+    sign_out @official_user
+    sign_in @resident_user
+    get category_url(@category)
+    assert_response :redirect
+    sign_out @resident_user
+  end
+
+  test "should get edit if admin" do
     sign_in @admin_user
     get edit_category_url(@category)
     assert_response :success
+  end
+
+  test "should not get edit if official, resident, or unauthenticated" do
+    get edit_category_url(@category)
+    assert_response :redirect
+    sign_in @official_user
+    get edit_category_url(@category)
+    assert_response :redirect
+    sign_out @official_user
+    sign_in @resident_user
+    get edit_category_url(@category)
+    assert_response :redirect
+    sign_out @resident_user
   end
 
   test "should update category" do
