fff5fb827f31b46b8ee14ef2008a179178dd3595
Amber Suratte <as00193@my.westga.edu>
2022-07-02 16:13:35 -0500

    Removed admin login routing test from officials controller tests and added to home controller test. Refactored official login routing test.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,6 +1,18 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @admin_user = users(:three)
+  end
+
+  test "admin should go to homepage on login" do
+    sign_in @admin_user
+    post user_session_url
+    assert_redirected_to root_path
+  end
+  
   test "should get index" do
     get root_url
     assert_response :success
