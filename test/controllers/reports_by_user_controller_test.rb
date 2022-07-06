436dc16a24681879431beb298d29b4e7500bf78a
Amber Suratte <as00193@my.westga.edu>
2022-07-06 18:08:24 -0500

    Added unit tests for ReportsByUser controller to confirm that admins and officials are the only users that can access Reports by User feature.

--- test/controllers/reports_by_user_controller_test.rb
+++ test/controllers/reports_by_user_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class ReportsByUserControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @resident_user = users(:one)
+    @official_user = users(:two)
+    @admin_user = users(:three)
+  end
+
+  test "admin and official users should get reports-by-user" do
+    sign_in @official_user
+    get reports_by_user_url, params: { user_id: @resident_user.id }
+    assert_response :success
+    sign_out @official_user
+    sign_in @admin_user
+    get reports_by_user_url, params: { user_id: @resident_user.id }
+    assert_response :success
+  end
+
+  test "resident and unauthenticated users should not get reports-by-user" do
+    get reports_by_user_url, params: { user_id: @resident_user.id }
+    assert_response :redirect
+    sign_in @resident_user
+    get reports_by_user_url, params: { user_id: @resident_user.id }
+    assert_response :redirect
+    sign_out @resident_user
+  end
+
 end
