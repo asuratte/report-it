65d0099673b3ccd25a3d107d31135e91864431af
Amber Suratte <as00193@my.westga.edu>
2022-07-11 06:47:47 -0500

    Generated controller for KPI Dashboard

---
+++ app/controllers/kpi_dashboard_controller.rb
@@ -0,0 +1,2 @@
+class KpiDashboardController < ApplicationController
+end
---
+++ test/controllers/kpi_dashboard_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class KpiDashboardControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
