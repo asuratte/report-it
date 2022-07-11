3b5aedfe354ef49d4f4a247401860a66b2b9eab0
Amber Suratte <as00193@my.westga.edu>
2022-07-11 13:45:32 -0500

    Added search form for KPI report

--- app/controllers/kpi_dashboard_controller.rb
+++ app/controllers/kpi_dashboard_controller.rb
@@ -1,5 +1,17 @@
 class KpiDashboardController < ApplicationController
     def index
-        @reports = Report.all
+        if params[:start_date].present? && params[:end_date].present?
+            @start_date = Date.parse(params[:start_date])
+            start_date_beginning_of_day = @start_date.beginning_of_day
+            @end_date = Date.parse(params[:end_date])
+            end_date_end_of_day = @end_date.end_of_day
+            puts @start_date
+            puts @end_date
+            @reports = Report.where(created_at: (start_date_beginning_of_day)..(end_date_end_of_day))
+        else
+            @reports = nil
+            @start_date = nil
+            @end_date = nil
+        end
     end
 end
