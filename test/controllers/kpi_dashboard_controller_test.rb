87f6d1d36eb5843547e95b862f4e56b1c2e5a3d1
Amber Suratte <as00193@my.westga.edu>
2022-07-11 19:10:38 -0500

    Added date validity unit tests for KPI dashboard

--- test/controllers/kpi_dashboard_controller_test.rb
+++ test/controllers/kpi_dashboard_controller_test.rb
@@ -28,7 +28,7 @@ class KpiDashboardControllerTest < ActionDispatch::IntegrationTest
     sign_out @resident_user
   end
 
-  test "should show charts on 'choose dates' button click when start and end date are provided" do
+  test "should show charts on 'choose dates' button click when start and end date are provided and reports exist for range" do
     sign_in @admin_user
     @start_date = DateTime.current.beginning_of_day.to_s
     @end_date = (DateTime.current.beginning_of_day + 1.year).to_s
@@ -37,7 +37,16 @@ class KpiDashboardControllerTest < ActionDispatch::IntegrationTest
     assert_select ".total-submitted h2", text: "Total Number of Reports Submitted"
   end
 
-  test "should show charts on 'view all time' button click when start and end date are provided" do
+  test "should show charts on 'choose dates' button click when start and end date are equal and reports exist for range" do
+    sign_in @admin_user
+    @start_date = DateTime.current.beginning_of_day.to_s
+    @end_date = DateTime.current.beginning_of_day.to_s
+    get '/kpi-dashboard?start_date=' + @start_date + '&end_date=' + @end_date + '&commit=Choose+Dates'
+    assert_response :success
+    assert_select ".total-submitted h2", text: "Total Number of Reports Submitted"
+  end
+
+  test "should show charts on 'view all time' button click when start and end date are provided and reports exist" do
     sign_in @admin_user
     @start_date = DateTime.current.beginning_of_day.to_s
     @end_date = (DateTime.current.beginning_of_day + 1.year).to_s
@@ -67,7 +76,17 @@ class KpiDashboardControllerTest < ActionDispatch::IntegrationTest
     get '/kpi-dashboard?start_date=' + @start_date + '&end_date=' + @end_date + '&commit=Choose+Dates'
     assert_response :success
     assert_select ".total-submitted h2", false, text: "Total Number of Reports Submitted"
-    assert_select ".text-center.fst-italic", "No data found for #{@start_date.to_date.to_s} - #{@end_date.to_date.to_s}."
+    assert_select ".info-message", "No data found for #{@start_date.to_date.to_s} - #{@end_date.to_date.to_s}."
+  end
+
+  test "should not show charts on 'choose dates' button click when start date is after end date" do
+    sign_in @admin_user
+    @start_date = (DateTime.current.beginning_of_day + 1.year).to_s
+    @end_date = (DateTime.current.beginning_of_day + 1.month).to_s
+    get '/kpi-dashboard?start_date=' + @start_date + '&end_date=' + @end_date + '&commit=Choose+Dates'
+    assert_response :success
+    assert_select ".total-submitted h2", false, text: "Total Number of Reports Submitted"
+    assert_select ".info-message", "Please enter a valid start and end date."
   end
 
   test "should hide charts on 'clear selection' button click" do
