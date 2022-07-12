4ffac02ca622d6713469928cef0f4778acf43f55
Amber Suratte <as00193@my.westga.edu>
2022-07-12 07:00:27 -0500

    Updated KPI controller to indicate whether clear button should be disabled for each case. Added disabled option to Clear Selection submit button. Changed Clear Selection button to secondary.

--- app/controllers/kpi_dashboard_controller.rb
+++ app/controllers/kpi_dashboard_controller.rb
@@ -7,19 +7,23 @@ class KpiDashboardController < ApplicationController
             end_date_end_of_day = @end_date.end_of_day
             @reports = Report.where(created_at: (start_date_beginning_of_day)..(end_date_end_of_day))
             @invalid_date = false
+            @selection_cleared = false
         elsif params[:commit] == 'Choose Dates' && ((!params[:start_date].present? || !params[:end_date].present?) || params[:start_date] > params[:end_date])
             @invalid_date = true
+            @selection_cleared = true
         elsif params[:commit] == 'View All Time'
             @reports = Report.all
             @invalid_date = false
         elsif params[:commit] == 'Clear Selection'
             redirect_to kpi_dashboard_path
             @invalid_date = false
+            @selection_cleared = true
         else
             @reports = nil
             @start_date = nil
             @end_date = nil
             @invalid_date = false
+            @selection_cleared = true
         end
     end
 end
