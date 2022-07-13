6e1aaed944890d47663415c346a1b31fa2809816
Amber Suratte <as00193@my.westga.edu>
2022-07-13 07:12:42 -0500

    Added deactivated_reports variable to KPI controller index action where needed for providing list of deactivated reports. Updated views to reflect changes. Modified KPI charts partial to correctly access deactivated reports for Nubmer of Deactivated Reports chart.

--- app/controllers/kpi_dashboard_controller.rb
+++ app/controllers/kpi_dashboard_controller.rb
@@ -5,7 +5,8 @@ class KpiDashboardController < ApplicationController
             start_date_beginning_of_day = @start_date.beginning_of_day
             @end_date = Date.parse(params[:end_date])
             end_date_end_of_day = @end_date.end_of_day
-            @reports = Report.where(created_at: (start_date_beginning_of_day)..(end_date_end_of_day))
+            @new_reports = Report.where(created_at: (start_date_beginning_of_day)..(end_date_end_of_day))
+            @deactivated_reports = Report.where(deactivated_at: (start_date_beginning_of_day)..(end_date_end_of_day)).where.not(deactivated_at: nil)
             @users = User.all.where(created_at: (start_date_beginning_of_day)..(end_date_end_of_day))
             @invalid_date = false
             @selection_cleared = false
@@ -15,9 +16,11 @@ class KpiDashboardController < ApplicationController
             @selection_cleared = true
             @all_time = false
         elsif params[:commit] == 'View All Time'
-            @reports = Report.all
+            @new_reports = Report.all
+            @deactivated_reports = Report.all.where.not(deactivated_at: nil)
             @users = User.all.where(created_at: (start_date_beginning_of_day)..(end_date_end_of_day))
             @invalid_date = false
+            @selection_cleared = false
             @all_time = true
         elsif params[:commit] == 'Clear Selection'
             redirect_to kpi_dashboard_path
@@ -25,7 +28,7 @@ class KpiDashboardController < ApplicationController
             @selection_cleared = true
             @all_time = false
         else
-            @reports = nil
+            @new_reports = nil
             @start_date = nil
             @end_date = nil
             @invalid_date = false
