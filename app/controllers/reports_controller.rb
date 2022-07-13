10b4b074e343e1fcdd3fb4a4b41780b926085c8b
Mike Bastarache <mb00147@my.westga.edu>
2022-07-13 19:21:30 -0400

    Created search concern, moved shared search methods from controller to concern

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,64 +5,28 @@ class ReportsController < ApplicationController
   before_action :set_report, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: %i[ index show ]
   before_action :allow_report_access, only: %i[ edit update destroy ]
-  before_action :get_search_values, only: [:index]
 
   # GET /reports or /reports.json
   def index
+    @search_page = :resident
+    get_search_values @search_page
+    get_search_categories @search_page
     @search_submit_path = reports_path
 
-    if params[:commit] == 'Clear Attribute'
-      self.set_submit_fields('clear')
+    if params[:commit] == 'Clear'
+      self.set_submit_fields('clear', @search_page)
       @pagy, @reports = pagy(Report.order('created_at DESC').where(active_status: 0), items: 10, size: [1,0,0,1])
+      params[:search_radio_value] == 'Attribute' ? self.set_radio_div('attribute') : self.set_radio_div('dates')
 
-      @reports_cleared = true
-      self.set_radio_div('attribute')
-
-    elsif params[:commit] == 'Clear Dates'
-      self.set_submit_fields('clear')
-      @pagy, @reports = pagy(Report.order('created_at DESC').where(active_status: 0), items: 10, size: [1,0,0,1])
-
-      @reports_cleared = true
-      self.set_radio_div('dates')
-
-    elsif params[:commit] == 'Search Dates' && session[:resident_start_date].present? && session[:resident_end_date].present?
-      self.set_submit_fields('dates')
+    elsif params[:commit] == 'Search Dates'
+      self.set_submit_fields('dates', @search_page)
       @pagy, @reports = pagy(Report.order('created_at DESC').search_dates(session[:resident_start_date], session[:resident_end_date]).where(active_status: 0), items: 10, size: [1,0,0,1])
-
-      @reports_cleared = false
       self.set_radio_div('dates')
-    elsif params[:commit] == 'Search Dates' && !session[:resident_start_date].present? && !session[:resident_end_date].present?
-      self.set_submit_fields('dates')
 
-      @pagy, @reports = pagy(Report.order('created_at DESC').where(active_status: 0), items: 10, size: [1,0,0,1])
-
-      @reports_cleared = true
-      self.set_radio_div('dates')
     else
-      self.set_submit_fields('attribute')
+      self.set_submit_fields('attribute', @search_page)
       @pagy, @reports = pagy(Report.order('created_at DESC').search(session[:resident_search_type], session[:resident_search_term]).where(active_status: 0), items: 10, size: [1,0,0,1])
-
-      @reports_cleared = false
       self.set_radio_div('attribute')
-    end
-  end
-
-  def set_submit_fields(set_submit_type)
-    if set_submit_type == 'clear'
-      @resident_search_type = nil
-      @resident_search_term = nil
-      @resident_start_date = nil
-      @resident_end_date = nil
-    elsif set_submit_type == 'attribute'
-      @resident_search_type = session[:resident_search_type]
-      @resident_search_term = session[:resident_search_term]
-      @resident_start_date = nil
-      @resident_end_date = nil
-    elsif set_submit_type == "dates"
-      @resident_search_type = nil
-      @resident_search_term = nil
-      @resident_start_date = session[:resident_start_date]
-      @resident_end_date = session[:resident_end_date]
     end
   end
 
@@ -183,19 +147,5 @@ class ReportsController < ApplicationController
         redirect_to root_path
       end
     end
-
-    # Sets the search type and term for the session using the search parameters
-    def get_search_values
-      session[:resident_search_type] = nil
-      session[:resident_search_term] = nil
-      session[:resident_start_date] = nil
-      session[:resident_end_date] = nil
-
-      if params[:resident_search_term] || (params[:resident_start_date] && params[:resident_end_date])
-        session[:resident_search_type] = params[:resident_search_type]
-        session[:resident_search_term] = params[:resident_search_term]
-        session[:resident_start_date] = params[:resident_start_date]
-        session[:resident_end_date] = params[:resident_end_date]
-      end
-    end
+    
 end
