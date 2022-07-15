cf99246d80dd1ff798ebf1b9e01344b9b45aa2f0
Amber Suratte <as00193@my.westga.edu>
2022-07-15 10:14:55 -0500

    Removed unnecessary 'where' clauses from variable assignments for 'all time' in KPI controller, added additional as needed to improve specificity in selection.

--- app/controllers/kpi_dashboard_controller.rb
+++ app/controllers/kpi_dashboard_controller.rb
@@ -6,17 +6,17 @@ class KpiDashboardController < ApplicationController
             @end_date = Date.parse(params[:end_date])
             end_date_end_of_day = @end_date.end_of_day
             @new_reports = Report.where(created_at: (start_date_beginning_of_day)..(end_date_end_of_day))
-            @deactivated_reports = Report.where(deactivated_at: (start_date_beginning_of_day)..(end_date_end_of_day)).where.not(deactivated_at: nil)
+            @deactivated_reports = Report.where(deactivated_at: (start_date_beginning_of_day)..(end_date_end_of_day)).where.not(deactivated_at: nil).where.not(active_status: 'active')
             @new_users = User.all.where(role: 'resident').where(created_at: (start_date_beginning_of_day)..(end_date_end_of_day))
-            @deactivated_users = User.all.where(role: 'resident').where.not(deactivated_at: nil).where(deactivated_at: (start_date_beginning_of_day)..(end_date_end_of_day))
+            @deactivated_users = User.all.where(role: 'resident').where.not(deactivated_at: nil).where(deactivated_at: (start_date_beginning_of_day)..(end_date_end_of_day)).where(active: false)
         elsif params[:commit] == 'Choose Dates' && ((!params[:start_date].present? || !params[:end_date].present?) || params[:start_date] > params[:end_date])
             @invalid_date = true
             @selection_cleared = true
         elsif params[:commit] == 'View All Time'
             @new_reports = Report.all
-            @deactivated_reports = Report.all.where.not(deactivated_at: nil)
-            @new_users = User.all.where(role: 'resident').where(created_at: (start_date_beginning_of_day)..(end_date_end_of_day))
-            @deactivated_users = User.all.where(role: 'resident').where.not(deactivated_at: nil).where(deactivated_at: (start_date_beginning_of_day)..(end_date_end_of_day))
+            @deactivated_reports = Report.all.where.not(deactivated_at: nil).where.not(active_status: 'active')
+            @new_users = User.all.where(role: 'resident')
+            @deactivated_users = User.all.where(role: 'resident').where.not(deactivated_at: nil).where(active: false)
             @all_time = true
         elsif params[:commit] == 'Clear Selection'
             redirect_to kpi_dashboard_path
